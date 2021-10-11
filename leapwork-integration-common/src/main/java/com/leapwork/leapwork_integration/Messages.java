186b4babd285463645349ab8443434d32bf90b53
BirSikanderMahajan <bsi@leapwork.com>
2021-10-11 16:51:01 +0530

    removing ! sign from messages

--- leapwork-integration-common/src/main/java/com/leapwork/leapwork_integration/Messages.java
+++ leapwork-integration-common/src/main/java/com/leapwork/leapwork_integration/Messages.java
@@ -3,18 +3,18 @@ package com.leapwork.leapwork_integration;
 public final class Messages {
 
     public static final String SCHEDULE_DETECTED = "Schedule %1$s[%2$s] successfully detected";
-    public static final String SCHEDULE_RUN_SUCCESS = "Schedule %1$s[%2$s] Launched Successfully!";
+    public static final String SCHEDULE_RUN_SUCCESS = "Schedule %1$s[%2$s] Launched Successfully.";
 
     public static final String SCHEDULE_TITLE_OR_ID_ARE_NOT_GOT = "Tried to get schedule title or id";
     public static final String SCHEDULE_RUN_FAILURE = "Failed to run %1$s[%2$s]";
-    public static final String NO_SUCH_SCHEDULE = "No such schedule! This may occur if try to run schedule that controller does not have. It can be deleted. Or you simply have forgotten to select schedules after changing controller address;";
+    public static final String NO_SUCH_SCHEDULE = "No such schedule, This may occur if try to run schedule that controller does not have. It can be deleted. Or you simply have forgotten to select schedules after changing controller address;";
     public static final String NO_SUCH_SCHEDULE_WAS_FOUND = "Could not find %1$s[%2$s] schedule It was likely deleted";
     public static final String NO_SUCH_RUN_WAS_FOUND = "Could not find run %1$s of %2$s schedule";
     public static final String NO_SUCH_RUN_ITEM_WAS_FOUND = "Could not find run item %1$s of %2$s schedule";
-    public static final String NO_SUCH_RUN = "No such run %1$s!";
+    public static final String NO_SUCH_RUN = "No such run %1$s";
 
-    public static final String REPORT_FILE_NOT_FOUND = "Couldn't find report file! Wrong path, Press \"help\" button nearby \"report\" textbox";
-    public static final String REPORT_FILE_CREATION_FAILURE = "Failed to create a report file!";
+    public static final String REPORT_FILE_NOT_FOUND = "Couldn't find report file. Wrong path, Press \"help\" button nearby \"report\" textbox";
+    public static final String REPORT_FILE_CREATION_FAILURE = "Failed to create a report file.";
 
     public static final String SCHEDULE_CONSOLE_LOG_SEPARATOR = "//////////////////////////////////////////////////////////////////////////////////////";
     public static final String CASE_CONSOLE_LOG_SEPARATOR = "----------------------------------------------------------------------------------------";
@@ -38,7 +38,7 @@ public final class Messages {
     public static final String SCHEDULE_VARIABLE_REQUEST_PART = "Schedule variables request part: %1$s";
 
 
-    public static final String NO_SCHEDULES = "No Schedules to run! All schedules you've selected could be deleted. Or you simply have forgotten to select schedules after changing controller address;";
+    public static final String NO_SCHEDULES = "No Schedules to run, All schedules you've selected could be deleted. Or you simply have forgotten to select schedules after changing controller address;";
 
     public static final String PLUGIN_SUCCESSFUL_FINISH = "Leapwork for Teamcity  plugin  successfully finished";
     public static final String PLUGIN_ERROR_FINISH = "Leapwork for Teamcity plugin finished with errors";
@@ -47,8 +47,8 @@ public final class Messages {
     public static final String PLEASE_CONTACT_SUPPORT = "If nothing helps, please contact support https://leapwork.com/support and provide the next information:\n1.Plugin Logs\n2.Leapwork and plugin version\n3.Controller logs from the moment you've run the plugin.\n4.Assets without videos if possible.\nYou can find them {Path to Leapwork}/Leapwork/Assets\nThank you";
 
     public static final String ERROR_CODE_MESSAGE = "Code: %1$s Status: %2$s";
-    public static final String COULD_NOT_CONNECT_TO = "Could not connect to %1$s! Check it and try again";
-    public static final String COULD_NOT_CONNECT_TO_BUT_WAIT = "Could not connect to %1$s! Check connection, The plugin is waiting for connection reestablishment";
+    public static final String COULD_NOT_CONNECT_TO = "Could not connect to %1$s, Check it and try again";
+    public static final String COULD_NOT_CONNECT_TO_BUT_WAIT = "Could not connect to %1$s, Check connection, The plugin is waiting for connection reestablishment";
     public static final String INTERRUPTED_EXCEPTION = "Interrupted exception: %1$s";
     public static final String EXECUTION_EXCEPTION = "Execution exception: %1$s";
     public static final String IO_EXCEPTION = "I/O exception: %1$s";
